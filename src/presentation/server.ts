4c5aeb4a94e561db8f8dc39bde3ec6ebade83134
Alejoboga20 <alejoboga19@gmail.com>
2024-06-10 17:45:57 -0300

    refactor: Add file system repository and update server to use it

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,9 @@
 import { CheckService } from '../domain/use-cases/ checks/check-service';
+import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
+import { LogRepositoryImplementation } from '../infrastructure/repositories/log.repository';
 import { CronService } from './cron/cron-service';
+
+const fileSystemRepository = new LogRepositoryImplementation(new FileSystemDatasource());
 
 export class Server {
 	public static start() {
@@ -8,9 +12,11 @@ export class Server {
 
 		const job = CronService.createJob('*/5 * * * * *', () =>
 			new CheckService(
+				fileSystemRepository,
 				() => console.log('Success callback: ', url),
 				(error) => console.log(`Error callback: ${error}`)
 			).execute(url)
 		);
+		job.start();
 	}
 }
