e8af6c8915cd6c5694b7954d375d9b3ed96a9ef6
Alejoboga20 <alejoboga19@gmail.com>
2024-06-13 18:23:13 -0300

    send mail use case

--- src/presentation/email/email.service.ts
+++ src/presentation/email/email.service.ts
@@ -1,7 +1,5 @@
 import nodemailer from 'nodemailer';
 import { envs } from '../../config/plugins/envs.plugin';
-import { LogRepository } from '../../domain/repository/log.repository';
-import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity';
 
 interface SendMailOptions {
 	to: string | string[];
@@ -34,8 +32,6 @@ const ATTACHMENTS: Attachment[] = [
 ];
 
 export class EmailService {
-	constructor(private readonly logRepository: LogRepository) {}
-
 	private transporter = nodemailer.createTransport({
 		service: envs.MAILER_SERVICE,
 		auth: {
@@ -56,32 +52,20 @@ export class EmailService {
 				attachments,
 			});
 
-			const log = new LogEntity({
-				level: LogSeverityLevel.LOW,
-				message: `Email sent to ${to}`,
-				origin: 'EmailService',
-			});
-
-			this.logRepository.saveLog(log);
 			return true;
 		} catch (error) {
-			const log = new LogEntity({
-				level: LogSeverityLevel.HIGH,
-				message: `Email not sent to ${to}`,
-				origin: 'EmailService',
-			});
-
-			this.logRepository.saveLog(log);
 			return false;
 		}
 	}
 
 	async sendEmailWithFileSystemLogs(to: string | string[]) {
-		await this.sendMail({
+		const sentInformation = await this.sendMail({
 			to,
 			subject: SUBJECT,
 			htmlBody: HTML_BODY,
 			attachments: ATTACHMENTS,
 		});
+
+		return sentInformation;
 	}
 }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,18 +1,20 @@
 import { CheckService } from '../domain/use-cases/ checks/check-service';
+import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
 import { LogRepositoryImplementation } from '../infrastructure/repositories/log.repository';
 import { CronService } from './cron/cron-service';
 import { EmailService } from './email/email.service';
 
 const fileSystemRepository = new LogRepositoryImplementation(new FileSystemDatasource());
+const emailService = new EmailService();
 
 export class Server {
 	public static start() {
 		console.log('Server started...');
 		const url = 'https://www.google.com';
 
-		const emailService = new EmailService(fileSystemRepository);
-		emailService.sendEmailWithFileSystemLogs('alejo@zelta.ai');
+		const sendEmail = new SendEmailLogs(emailService, fileSystemRepository);
+		sendEmail.execute('alejo@zelta.ai');
 
 		const job = CronService.createJob('*/5 * * * * *', () =>
 			new CheckService(
