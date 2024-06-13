e3f6c18659ae3f10883b2d3e074274f04270ac06
Alejoboga20 <alejoboga19@gmail.com>
2024-06-13 12:09:04 -0300

    mail with nodemailer

---
+++ src/presentation/email/email.service.ts
@@ -0,0 +1,36 @@
+import nodemailer from 'nodemailer';
+import { envs } from '../../config/plugins/envs.plugin';
+
+interface SendMailOptions {
+	to: string;
+	subject: string;
+	htmlBody: string;
+	// attachments: any[];
+}
+
+export class EmailService {
+	private transporter = nodemailer.createTransport({
+		service: envs.MAILER_SERVICE,
+		auth: {
+			user: envs.MAILER_MAIL,
+			pass: envs.MAILER_SECRET_KEY,
+		},
+	});
+
+	async sendMail(options: SendMailOptions): Promise<boolean> {
+		const { to, subject, htmlBody } = options;
+		try {
+			const sentInformation = await this.transporter.sendMail({
+				to,
+				subject,
+				html: htmlBody,
+				from: envs.MAILER_MAIL,
+			});
+
+			console.log({ sentInformation });
+			return true;
+		} catch (error) {
+			return false;
+		}
+	}
+}
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,6 +2,7 @@ import { CheckService } from '../domain/use-cases/ checks/check-service';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
 import { LogRepositoryImplementation } from '../infrastructure/repositories/log.repository';
 import { CronService } from './cron/cron-service';
+import { EmailService } from './email/email.service';
 
 const fileSystemRepository = new LogRepositoryImplementation(new FileSystemDatasource());
 
@@ -10,6 +11,17 @@ export class Server {
 		console.log('Server started...');
 		const url = 'https://www.google.com';
 
+		const emailService = new EmailService();
+
+		emailService.sendMail({
+			to: 'alejo@zelta.ai',
+			subject: 'Server started',
+			htmlBody: `
+      <h3>Server started</h3>
+      <p>Server started at ${new Date().toISOString()}</p>
+      `,
+		});
+
 		const job = CronService.createJob('*/5 * * * * *', () =>
 			new CheckService(
 				fileSystemRepository,
@@ -17,6 +29,6 @@ export class Server {
 				(error) => console.log(`Error callback: ${error}`)
 			).execute(url)
 		);
-		job.start();
+		// job.start();
 	}
 }
