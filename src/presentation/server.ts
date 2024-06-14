7b25d423f108bc542a239b27d5edd16b675f78a4
Alejoboga20 <alejoboga19@gmail.com>
2024-06-14 12:26:36 -0300

    save logs in mongodb

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,11 +1,13 @@
 import { CheckService } from '../domain/use-cases/ checks/check-service';
 import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
+import { MongoLogDataSource } from '../infrastructure/datasources/mongo-log.datasource';
 import { LogRepositoryImplementation } from '../infrastructure/repositories/log.repository';
 import { CronService } from './cron/cron-service';
 import { EmailService } from './email/email.service';
 
-const fileSystemRepository = new LogRepositoryImplementation(new FileSystemDatasource());
+// const logRepository = new LogRepositoryImplementation(new FileSystemDatasource());
+const logRepository = new LogRepositoryImplementation(new MongoLogDataSource());
 const emailService = new EmailService();
 
 export class Server {
@@ -13,16 +15,16 @@ export class Server {
 		console.log('Server started...');
 		const url = 'https://www.google.com';
 
-		const sendEmail = new SendEmailLogs(emailService, fileSystemRepository);
+		const sendEmail = new SendEmailLogs(emailService, logRepository);
 		// sendEmail.execute('alejo@zelta.ai');
 
 		const job = CronService.createJob('*/5 * * * * *', () =>
 			new CheckService(
-				fileSystemRepository,
+				logRepository,
 				() => console.log('Success callback: ', url),
 				(error) => console.log(`Error callback: ${error}`)
 			).execute(url)
 		);
-		// job.start();
+		job.start();
 	}
 }
