8dad9db8d4fdef9f49fd34b08a27e23af2fb7108
Alejoboga20 <alejoboga19@gmail.com>
2024-06-14 12:06:21 -0300

    create and read using mongoose models

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { envs } from './config/plugins/envs.plugin';
-import { MongoDatabase } from './data/mongo';
+import { LogModel, MongoDatabase } from './data/mongo';
 import { Server } from './presentation/server';
 
 (async () => {
@@ -11,6 +11,13 @@ async function main() {
 		mongoUrl: envs.MONGO_URL,
 		dbName: envs.MONGO_DB_NAME,
 	});
+
+	// const newLog = await LogModel.create({
+	// 	message: 'Server started!',
+	// 	level: 'low',
+	// });
+	// await newLog.save();
+	// const logs = await LogModel.find();
 
 	Server.start();
 }
