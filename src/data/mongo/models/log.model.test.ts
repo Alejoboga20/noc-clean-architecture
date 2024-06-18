1b0f5d759590a206dd1c56ef33d29041d82825db
Alejoboga20 <alejoboga19@gmail.com>
2024-06-18 11:46:44 -0300

    test mongoose model

---
+++ src/data/mongo/models/log.model.test.ts
@@ -0,0 +1,59 @@
+import { envs } from '../../../config/plugins/envs.plugin';
+import { MongoDatabase } from '../init';
+import { LogModel } from './log.model';
+
+const logData = {
+	message: 'test',
+	origin: 'test',
+	level: 'low',
+	createdAt: new Date(),
+};
+
+describe('Log Model tests', () => {
+	beforeAll(async () => {
+		await MongoDatabase.connect({
+			mongoUrl: envs.MONGO_URL,
+			dbName: envs.MONGO_DB_NAME,
+		});
+	});
+
+	afterAll(async () => {
+		await MongoDatabase.disconnect();
+	});
+
+	test('should return LogModel', async () => {
+		const log = await LogModel.create(logData);
+
+		expect(log).toEqual(
+			expect.objectContaining({
+				...logData,
+			})
+		);
+	});
+
+	test('should return the schmea object', () => {
+		const schema = LogModel.schema.obj;
+
+		expect(schema).toEqual(
+			expect.objectContaining({
+				message: {
+					type: String,
+					required: true,
+				},
+				origin: {
+					type: String,
+					default: 'undefined',
+				},
+				level: {
+					type: String,
+					enum: ['low', 'medium', 'high'],
+					default: 'low',
+				},
+				createdAt: {
+					type: Date,
+					default: expect.any(Date),
+				},
+			})
+		);
+	});
+});
